d26575f67c758693c6ca538b15f9d181c8827121
Roman Golubovsky <romang@titanium-labs.com>
2021-03-02 19:37:32 +0200

    init

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const { sha3, BN } = require("web3-utils");
-const abiCoder = require("web3-eth-abi");
+const { sha3, soliditySha3, BN } = require('web3-utils');
+const abiCoder = require('web3-eth-abi');
 
 const state = {
   savedABIs: [],
@@ -11,27 +11,23 @@ function _getABIs() {
 }
 
 function _typeToString(input) {
-  if (input.type === "tuple") {
-    return "(" + input.components.map(_typeToString).join(",") + ")";
+  if (input.type === 'tuple') {
+    return '(' + input.components.map(_typeToString).join(',') + ')';
+  }
+  if (input.type === 'tuple[]') {
+    return '(' + input.components.map(_typeToString).join(',') + ')[]';
   }
   return input.type;
 }
 
 function _addABI(abiArray) {
-
   if (Array.isArray(abiArray)) {
     // Iterate new abi to generate method id"s
-    abiArray.map(function(abi) {
+    abiArray.map(function (abi) {
       if (abi.name) {
-        const signature = sha3(
-          abi.name +
-            "(" +
-            abi.inputs
-              .map(_typeToString)
-              .join(",") +
-            ")"
-        );
-        if (abi.type === "event") {
+        const toHash = abi.name + '(' + abi.inputs.map(_typeToString).join(',') + ')';
+        const signature = soliditySha3(toHash);
+        if (abi.type === 'event') {
           state.methodIDs[signature.slice(2)] = abi;
         } else {
           state.methodIDs[signature.slice(2, 10)] = abi;
@@ -41,26 +37,26 @@ function _addABI(abiArray) {
 
     state.savedABIs = state.savedABIs.concat(abiArray);
   } else {
-    throw new Error("Expected ABI array, got " + typeof abiArray);
+    throw new Error('Expected ABI array, got ' + typeof abiArray);
   }
 }
 
 function _removeABI(abiArray) {
   if (Array.isArray(abiArray)) {
     // Iterate new abi to generate method id"s
-    abiArray.map(function(abi) {
+    abiArray.map(function (abi) {
       if (abi.name) {
         const signature = sha3(
           abi.name +
-            "(" +
+            '(' +
             abi.inputs
-              .map(function(input) {
+              .map(function (input) {
                 return input.type;
               })
-              .join(",") +
-            ")"
+              .join(',') +
+            ')',
         );
-        if (abi.type === "event") {
+        if (abi.type === 'event') {
           if (state.methodIDs[signature.slice(2)]) {
             delete state.methodIDs[signature.slice(2)];
           }
@@ -72,7 +68,7 @@ function _removeABI(abiArray) {
       }
     });
   } else {
-    throw new Error("Expected ABI array, got " + typeof abiArray);
+    throw new Error('Expected ABI array, got ' + typeof abiArray);
   }
 }
 
@@ -94,15 +90,15 @@ function _decodeMethod(data) {
     for (let i = 0; i < decoded.__length__; i++) {
       let param = decoded[i];
       let parsedParam = param;
-      const isUint = abiItem.inputs[i].type.indexOf("uint") === 0;
-      const isInt = abiItem.inputs[i].type.indexOf("int") === 0;
-      const isAddress = abiItem.inputs[i].type.indexOf("address") === 0;
+      const isUint = abiItem.inputs[i].type.indexOf('uint') === 0;
+      const isInt = abiItem.inputs[i].type.indexOf('int') === 0;
+      const isAddress = abiItem.inputs[i].type.indexOf('address') === 0;
 
       if (isUint || isInt) {
         const isArray = Array.isArray(param);
 
         if (isArray) {
-          parsedParam = param.map(val => new BN(val).toString());
+          parsedParam = param.map((val) => new BN(val).toString());
         } else {
           parsedParam = new BN(param).toString();
         }
@@ -113,7 +109,7 @@ function _decodeMethod(data) {
         const isArray = Array.isArray(param);
 
         if (isArray) {
-          parsedParam = param.map(_ => _.toLowerCase());
+          parsedParam = param.map((_) => _.toLowerCase());
         } else {
           parsedParam = param.toLowerCase();
         }
@@ -131,77 +127,71 @@ function _decodeMethod(data) {
 }
 
 function _decodeLogs(logs) {
-  return logs.filter(log => log.topics.length > 0).map((logItem) => {
-    const methodID = logItem.topics[0].slice(2);
-    const method = state.methodIDs[methodID];
-    if (method) {
-      const logData = logItem.data;
-      let decodedParams = [];
-      let dataIndex = 0;
-      let topicsIndex = 1;
+  return logs
+    .filter((log) => log.topics.length > 0)
+    .map((logItem) => {
+      const methodID = logItem.topics[0].slice(2);
+      const method = state.methodIDs[methodID];
+      if (method) {
+        const logData = logItem.data;
+        let decodedParams = [];
+        let dataIndex = 0;
+        let topicsIndex = 1;
 
-      let dataTypes = [];
-      method.inputs.map(function(input) {
-        if (!input.indexed) {
-          dataTypes.push(input.type);
-        }
-      });
-
-      const decodedData = abiCoder.decodeParameters(
-        dataTypes,
-        logData.slice(2)
-      );
-
-      // Loop topic and data to get the params
-      method.inputs.map(function(param) {
-        let decodedP = {
-          name: param.name,
-          type: param.type,
-        };
-
-        if (param.indexed) {
-          decodedP.value = logItem.topics[topicsIndex];
-          topicsIndex++;
-        } else {
-          decodedP.value = decodedData[dataIndex];
-          dataIndex++;
-        }
-
-        if (param.type === "address") {
-          decodedP.value = decodedP.value.toLowerCase();
-          // 42 because len(0x) + 40
-          if (decodedP.value.length > 42) {
-            let toRemove = decodedP.value.length - 42;
-            let temp = decodedP.value.split("");
-            temp.splice(2, toRemove);
-            decodedP.value = temp.join("");
+        let dataTypes = [];
+        method.inputs.map(function (input) {
+          if (!input.indexed) {
+            dataTypes.push(input.type);
           }
-        }
+        });
 
-        if (
-          param.type === "uint256" ||
-          param.type === "uint8" ||
-          param.type === "int"
-        ) {
-          // ensure to remove leading 0x for hex numbers
-          if (typeof decodedP.value === "string" && decodedP.value.startsWith("0x")) {
-            decodedP.value = new BN(decodedP.value.slice(2), 16).toString(10);
+        const decodedData = abiCoder.decodeParameters(dataTypes, logData.slice(2));
+
+        // Loop topic and data to get the params
+        method.inputs.map(function (param) {
+          let decodedP = {
+            name: param.name,
+            type: param.type,
+          };
+
+          if (param.indexed) {
+            decodedP.value = logItem.topics[topicsIndex];
+            topicsIndex++;
           } else {
-            decodedP.value = new BN(decodedP.value).toString(10);
+            decodedP.value = decodedData[dataIndex];
+            dataIndex++;
           }
 
-        }
+          if (param.type === 'address') {
+            decodedP.value = decodedP.value.toLowerCase();
+            // 42 because len(0x) + 40
+            if (decodedP.value.length > 42) {
+              let toRemove = decodedP.value.length - 42;
+              let temp = decodedP.value.split('');
+              temp.splice(2, toRemove);
+              decodedP.value = temp.join('');
+            }
+          }
 
-        decodedParams.push(decodedP);
-      });
+          if (param.type === 'uint256' || param.type === 'uint8' || param.type === 'int') {
+            // ensure to remove leading 0x for hex numbers
+            if (typeof decodedP.value === 'string' && decodedP.value.startsWith('0x')) {
+              decodedP.value = new BN(decodedP.value.slice(2), 16).toString(10);
+            } else {
+              decodedP.value = new BN(decodedP.value).toString(10);
+            }
+          }
 
-      return {
-        name: method.name,
-        events: decodedParams,
-        address: logItem.address,
-      };
-    }
-  });
+          decodedParams.push(decodedP);
+        });
+
+        return {
+          name: method.name,
+          events: decodedParams,
+          address: logItem.address,
+        };
+      }
+    });
 }
 
 module.exports = {
